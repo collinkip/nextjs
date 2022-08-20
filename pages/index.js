ab172f7e36fbc6493c0933f1016ae15bb8dac58e
collinkip <collinskliplangat3@gmail.com>
2022-08-20 19:56:21 +0300

    replace and push routes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
+import Link from "next/link";
+import {useRouter} from 'next/router'
+
+
+
 function Home(){
-    return <h1>home page</h1>
+
+    const router=useRouter()
+    const handleDelete=()=>{
+        console.log('order placed');
+        router.push('/product')
+    }
+    return (
+     <div>
+         <h1>Home page</h1>
+        <Link href='/blog'><a><h2>Blog</h2></a>
+        </Link>
+        <Link href='/product'><a><h2>Product</h2></a>
+        </Link>
+
+        <button onClick={handleDelete}>
+           place an order
+        </button>
+    </div>
+    )
+    
 
 }
 
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,26 @@
-function ProductList(params) {
+import Link from "next/link";
+function ProductList({productid=100}) {
     return(
         <>
-        <h1>Product 1</h1>
-        <h1>Product 2</h1>
-        <h1>Product 3</h1>
+        <Link href='/'><a><h2>Home</h2></a>
+        </Link>
+        <h1><Link href="/product/1">
+        <a >Product 1</a>
+        </Link>
+        </h1>
+        <h1><Link href="/product/2">
+        <a >Product 2</a>
+        </Link>
+        </h1>
+        <h1><Link  href="/product/3" replace>
+        <a>Product 3</a>
+        </Link>
+        </h1>
+        <h1><Link  href={`/product/${productid}`}>
+        <a>Product {productid}</a>
+        </Link>
+        </h1>
+        
         
         </>
     )
